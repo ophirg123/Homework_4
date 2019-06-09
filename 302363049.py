ed8a5ff5a081edf7e7d15c09aeedd29bacdeff3b
ophir <gruteke@post.bgu.ac.il>
2019-06-09 21:54:28 +0300

    Finish

--- 302363049.py
+++ 302363049.py
@@ -30,10 +30,10 @@ class HydroCamel(auv_interface.Auv):
         self.mine_check()
 
     def create_sonar_fov(self, A_x, A_y, B_x, B_y, C_x, C_y):
-        for point_x in range(int(round(A_x)), (int(round(B_x)) + 1)):
+        for point_x in range(self.map_size[1]):
             print(point_x)
             P_x = point_x
-            for point_y in range(int(round(B_y)), (int(round(C_y)) + 1)):
+            for point_y in range(self.map_size[0]):
                 print(point_y)
                 P_y = point_y
                 W_1 = (A_x * (C_y - A_y) + (P_y - A_y) * (C_x - A_x) - P_x * (C_y - A_y)) / (
@@ -61,7 +61,7 @@ class HydroCamel(auv_interface.Auv):
         norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
         self.im = plt.imshow(self.map, interpolation='none', cmap=cmap, norm=norm)
         plt.show(block=False)
-        plt.pause(2)
+        plt.pause(3)
         plt.close()
         for sonar_point_y , sonar_point_x in self.sonar_fov_dictionary:
             if self.map[sonar_point_y, sonar_point_x] == 1:
@@ -77,6 +77,31 @@ class HydroCamel(auv_interface.Auv):
                 self.map[(sonar_point[0], sonar_point[1])] = 2
 
     def get_mines(self):
+
+        def quick_sort(lst):
+            if len(lst) <= 1:
+                return lst
+
+            pivot = lst[-1]
+            left_lst = []
+            right_lst = []
+            for i in range(len(lst) - 1):
+                if lst[i][1] < pivot[1]:
+                    left_lst.append(lst[i])
+                else:
+                    right_lst.append(lst[i])
+            return quick_sort(left_lst) + [pivot] + quick_sort(right_lst)
+
+        def bubble_sort(lst):
+            for k in range(len(lst)):
+                for i in range(len(lst) - 1 - k):
+                    if lst[i][0] > lst[i + 1][0] and lst[i][1] == lst[i + 1][1]:
+                        lst[i + 1], lst[i] = lst[i], lst[i + 1]
+            return lst
+
+        self.founded_mines = quick_sort(self.founded_mines)
+        self.founded_mines = bubble_sort(self.founded_mines)
+
         return self.founded_mines
 
     def get_sonar_fov(self):
@@ -92,18 +117,22 @@ class HydroCamel(auv_interface.Auv):
 
     def rotate_vectors(self, A_x, A_y, B_x, B_y, C_x, C_y, velocity):
         rotate_angle = np.arctan2(velocity[0][0], velocity[0][1]) * 180 / np.pi
+        current_angle = self.auv_angle
         if rotate_angle < 0:
             self.auv_angle = 360 + rotate_angle
         else:
             self.auv_angle = rotate_angle
-        rotate_angle_radian = np.arctan2(velocity[0][0], velocity[0][1])
+        if current_angle > self.auv_angle:
+            rotate_angle_radian = ((360+self.auv_angle)-current_angle)*np.pi/180
+        else:
+            rotate_angle_radian = (self.auv_angle-current_angle)*np.pi/180
         rotation_matrix = np.matrix([[np.cos(rotate_angle_radian), -np.sin(rotate_angle_radian)],[np.sin(rotate_angle_radian), np.cos(rotate_angle_radian)]])
-        B_point_after_rotate = (rotation_matrix.dot(np.array([B_x - A_x, B_y - A_x]))).tolist()
-        B_point_after_rotate_with_offset = (B_point_after_rotate[0][0] + A_x, B_point_after_rotate[0][1] + A_x)
+        B_point_after_rotate = (rotation_matrix.dot(np.array([B_x - A_x, B_y - A_y]))).tolist()
+        B_point_after_rotate_with_offset = (B_point_after_rotate[0][0] + A_x, B_point_after_rotate[0][1] + A_y)
         self.B_x = B_point_after_rotate_with_offset[0]
         self.B_y = B_point_after_rotate_with_offset[1]
-        C_point_after_rotate = (rotation_matrix.dot(np.array([C_x - A_x, C_y - A_x]))).tolist()
-        C_point_after_rotate_with_offset = (C_point_after_rotate[0][0] + A_x, C_point_after_rotate[0][1] + A_x)
+        C_point_after_rotate = (rotation_matrix.dot(np.array([C_x - A_x, C_y - A_y]))).tolist()
+        C_point_after_rotate_with_offset = (C_point_after_rotate[0][0] + A_x, C_point_after_rotate[0][1] + A_y)
         self.C_x = C_point_after_rotate_with_offset[0]
         self.C_y = C_point_after_rotate_with_offset[1]
 
@@ -126,22 +155,27 @@ class HydroCamel(auv_interface.Auv):
             self.duration_rotate.pop(0)
             self.velocity.pop(0)
 
-
-
-
+    def start(self):
+        while len(self.duration) > 0:
+            self.time_step()
+            self.display_map()
 
 
 
 
 if __name__ == "__main__":
 
-    # example 1
+    # # example 1
     # map_size = (20, 15)
     # mines = np.zeros(map_size).tolist()
     # mines[16][6] = 1
+    # mines[15][6] = 1
+    # mines[12][6] = 1
     # mines[12][4] = 1
     # mines[14][10] = 1
     # mines[17][11] = 1
+    # mines[18][11] = 1
+    # mines[14][11] = 1
     # velocity = list()
     # velocity.append([0, 1])
     # sonar_range = 6
@@ -159,7 +193,7 @@ if __name__ == "__main__":
     # print(game1.get_mines())
     # print(game1.get_sonar_fov())
     #
-    # # example 2
+     # example 2
     sonar_range = 5
     sonar_angle = 30
     map_size = (25, 20)
@@ -174,14 +208,18 @@ if __name__ == "__main__":
 
     game2 = HydroCamel(sonar_range, sonar_angle, map_size, initial_position, velocity, duration, mines)
     game2.display_map()
-    game2.time_step()
-    game2.display_map()
-    game2.time_step()
-    game2.display_map()
-    game2.time_step()
-    game2.display_map()
-    game2.time_step()
+    # game2.time_step()
+    # game2.display_map()
+    # game2.time_step()
+    # game2.display_map()
+    # game2.time_step()
+    # game2.display_map()
+    # game2.time_step()
+    # game2.display_map()
+    # game2.time_step()
+    # game2.display_map()
+    # game2.time_step()
+    game2.start()
     print(game2.get_mines())
     print(game2.get_sonar_fov())
 
-    # game2.start()
